10e2d55ff1174637e152550eef28d395154ff2fd
AkramAym <2391145@bdeb.qc.ca>
2024-12-09 20:49:20 -0500

    update

--- API_rest/server/logger.js
+++ API_rest/server/logger.js
@@ -1,6 +1,5 @@
 import winston from 'winston';
 
-// Définir différentes couleurs pour chaque niveau
 const colors = {
   error: "red",
   warn: "yellow",
@@ -9,19 +8,14 @@ const colors = {
   debug: "white",
 };
 
-// Personnaliser le format du log
 const format = winston.format.combine(
-  // Ajouter un horodatage avec le format préféré
   winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:ms" }),
-  // Indiquer à Winston que les logs doivent être colorés
   winston.format.colorize({ all: true }),
-  // Définir le format du message en affichant l'horodatage, le niveau et le message
   winston.format.printf(
     (info) => `${info.timestamp} ${info.level}: ${info.message}`
   )
 );
 
-// Associer les couleurs définies aux niveaux de sévérité
 winston.addColors(colors);
 
 const logger = winston.createLogger({
